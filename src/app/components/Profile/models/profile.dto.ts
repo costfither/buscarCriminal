c38b89843eccbb8afeac4e695ba8331b077e81bd
costfither <costabella_arnau@hotmail.com>
2022-01-10 20:42:02 +0100

    as

--- src/app/components/Profile/models/profile.dto.ts
+++ src/app/components/Profile/models/profile.dto.ts
@@ -7,17 +7,13 @@ export class ProfileDTO {
   skinColor: string | undefined; //color de la pell
   bornLocation: string | undefined; //lloc de naixement
   name: string | undefined; //nom
-  weight: number | undefined; //pes
   dateBorn: number | undefined; //data de naixement
-  tattooNumber: number | undefined; //numero de tatuatges
   surname2: string | undefined; //segon cognom
   ethnic_characteristics: string | undefined; // caracteristiques etniques
-  height: number | undefined; //alçada
   eyeColor: string | undefined; //color dels ulls
   deaf: boolean | undefined; //sord
   lackExtremitats: number | undefined; //falta de extremitats
   mute: boolean | undefined; //si es mud
-  footNumber: number | undefined; //numero de peu
   surname1: string | undefined; //primer cognom;
   age: number | undefined; //edat
   studies: string[] | undefined; //llistat d'estudis
--- src/app/components/Profile/profile-page/profile-page.component.ts
+++ src/app/components/Profile/profile-page/profile-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
@@ -12,6 +17,7 @@ import { ProfileDTO } from '../models/profile.dto';
 })
 export class ProfilePageComponent implements OnInit {
   profile: ProfileDTO;
+
   operation: FormControl; //operacions
   hairColor: FormControl; //color de cabell
   scars: FormControl; //cicatriu
@@ -19,17 +25,12 @@ export class ProfilePageComponent implements OnInit {
   skinColor: FormControl; //color de la pell
   bornLocation: FormControl; //lloc de naixement
   name: FormControl; //nom
-  weight: FormControl; //pes
   dateBorn: FormControl; //data de naixement
-  tattooNumber: FormControl; //numero de tatuatges
   surname2: FormControl; //segon cognom
-  ethnic_characteristics: FormControl; // caracteristiques etniques
-  height: FormControl; //alçada
   eyeColor: FormControl; //color dels ulls
   deaf: FormControl; //sord
   lackExtremitats: FormControl; //falta de extremitats
   mute: FormControl; //si es mud
-  footNumber: FormControl; //numero de peu
   surname1: FormControl; //primer cognom;
   age: FormControl; //edat
   studies: FormControl; //llistat d'estudis
@@ -47,6 +48,76 @@ export class ProfilePageComponent implements OnInit {
   ) {
     this.isValidForm = null;
     this.profileId = this.activatedRoute.snapshot.paramMap.get('profileID');
+    this.profile = new ProfileDTO('');
+    this.isUpdateMode = false;
+
+    this.name = new FormControl(this.profile.name, [
+      Validators.required,
+      Validators.maxLength(55),
+    ]);
+
+    this.surname1 = new FormControl(this.profile.surname1, [
+      Validators.required,
+      Validators.maxLength(255),
+    ]);
+    this.surname2 = new FormControl(this.profile.surname2, [
+      Validators.required,
+      Validators.maxLength(255),
+    ]);
+
+    this.publication_date = new FormControl(
+      formatDate(this.post.publication_date, 'yyyy-MM-dd', 'en'),
+      [Validators.required]
+    );
+
+    this.categories = new FormControl([]);
+
+    this.postForm = this.formBuilder.group({
+      title: this.title,
+      description: this.description,
+      publication_date: this.publication_date,
+      categories: this.categories,
+    });
+
+    this.store.select('auth').subscribe((auth) => {
+      if (auth.credentials.user_id) {
+        this.userId = auth.credentials.user_id;
+      }
+    });
+
+    this.store.select('categories').subscribe((categories) => {
+      this.categoriesList = categories.categories;
+    });
+
+    this.store.select('posts').subscribe((posts) => {
+      this.post = posts.post;
+
+      this.title.setValue(this.post.title);
+
+      this.description.setValue(this.post.description);
+
+      this.publication_date.setValue(
+        formatDate(this.post.publication_date, 'yyyy-MM-dd', 'en')
+      );
+
+      if (this.post.categories) {
+        let categoriesIds: string[] = [];
+        this.post.categories.forEach((cat: CategoryDTO) => {
+          categoriesIds.push(cat.categoryId);
+        });
+
+        this.categories.setValue(categoriesIds);
+      }
+
+      this.postForm = this.formBuilder.group({
+        title: this.title,
+        description: this.description,
+        publication_date: this.publication_date,
+        categories: this.categories,
+      });
+    });
+
+    this.loadCategories();
   }
 
   ngOnInit(): void {}
